658572c1cfb1fefb4e0a6a4129e87349e191f663
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-05-02 00:20:00 -0500

    80% de progreso, interacción lista

--- src/hooks/HuseEffect.js
+++ src/hooks/HuseEffect.js
@@ -1,98 +1,122 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import '../styles/Tarjetas.css'
+import {
+  AiOutlineCaretDown,
+  AiOutlineCaretUp
+} from "react-icons/ai";
 // se diseñan tarjetas
-
-
 
 
 export default function HuseEffect() {
   // se declara la varibale de estado para definir personaje y para actulizar estado
   const [personajes, setPersonajes] = useState([])
-  
+
+  // se declara la funcion que va a ser usada dentro de useEffect
+
+  const getData = async () => {
+    let url = 'https://rickandmortyapi.com/api/character/'
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    setPersonajes(data.results)
+  }
   // uso de useEffect
   useEffect(() => {
-    const getData = async () => {
-      const data=await fetch('https://rickandmortyapi.com/api/character/')
-      const json = await data.json()
-      console.log(json);
-      setPersonajes(json.results)
-      console.log(personajes)
-    }
-    getData()
-    //
-  
+    // se suscribe al evento
+    // montaje
+    window.addEventListener('load', getData)
     return () => {
-      //
+      // desmontaje
+      // se desuscribe al evento
+      window.removeEventListener('load', getData)
     }
   }, [])
-  
+
   return (
     <div>
-    Uso del hook useEffect
-    <div>
-      <ol className="container-tarjeta">
-        {
-          personajes.map(personaje => 
-          <Personaje
-          key = {uuidv4()}
-          name = {personaje.name}
-          imagen = {personaje.image}
-          specie = {personaje.species}
-          status = {personaje.status}
-          id = {uuidv4()}
-          estado = {personajes}
-          setEstado = {setPersonajes}
-          completed = {false}
-          /> 
-          )
-        }
-      </ol>
-    </div>
+      Uso del hook useEffect
+      <div>
+        <ol className="container-tarjeta">
+          {
+            personajes.map(personaje =>
+              <Personaje
+                key={uuidv4()}
+                name={personaje.name}
+                imagen={personaje.image}
+                specie={personaje.species}
+                status={personaje.status}
+                origin={personaje.location.name}
+                gender = {personaje.gender}
+              />
+            )
+          }
+        </ol>
+      </div>
     </div>
   )
 }
 
 
-function Personaje({imagen,name,specie,status,id,completed}){
-  // constante estilo
+function Personaje({ imagen, name, specie, status, origin, gender }) {
+  // constantes estilos
   const estiloLI = {
     listStyle: 'none',
-    marginTop: '0px',
+    marginTop: '10px',
     transition: 'all .5s ease',
   }
   const listStyleNO = {
     listStyle: 'none',
   }
+  // declacion de variable de estado
+  const [over, setOver] = useState(false)
+  // funcion toggle manipulando el estado
+  const filtrado = () => {
+    setOver(!over)
+  }
+  return (
+    // renderizado condicional, buscar como optimizarlo
+    over ?
 
-  const [over,setOver] = useState(false)
- 
-    const filtrado = ()=> {
-      
-      setOver(true)
-    }
-  return(
-    over ? 
-   <div className= 'tarjetaMain' onMouseOver={filtrado}
-   onMouseLeave = {()=> setOver(false)}>
+      <div className={over ? 'tarjetaMain reducirTamannio' : 'tarjetaMain aumentar'}>
+        <Button evento={filtrado}>
+          {over ? <AiOutlineCaretUp /> : <AiOutlineCaretDown />}
+        </Button>
+        <div
+
+          className='tarjeta' >
+          <li style={listStyleNO}>Hi, I'm  {name}</li>
+          <img className={over ? 'img-style reducirTamannio' : 'img-style aumentar'} src={imagen} alt={`imagen de ${name}`} />
+          {over ? <li style={estiloLI}>Especie: {specie}</li> : null}
+          {over ? <li>Estado: {status}</li> : null}
+          {over ? <li>Ubicación: {origin}</li> : null}
+          {over ? <li>Género: {gender}</li> : null}
+        </div>
+      </div>
+
+      :
+
+      <div className={over ? 'tarjetaMain reducirTamannio' : 'tarjetaMain aumentar'}>
+        <Button evento={filtrado}>
+          {over ? <AiOutlineCaretUp /> : <AiOutlineCaretDown />}
+        </Button>
+        <div
+          className='tarjeta'>
+          <li>Hi, I'm {name}</li>
+          <img className={over ? 'img-style reducirTamannio' : 'img-style aumentar'} src={imagen} alt={`imagen de ${name}`} />
+        </div>
+      </div>
+
+  )
+}
+
+const Button = (props) => {
+  return (
     <div
-    className= 'tarjeta' >
-      <li style = {{listStyle: 'none'}}>{name}</li>
-      <img className = {over ? 'img-style reducirTamannio' : 'img-style'} src = {imagen} alt= {`imagen de ${name}`}/>
-      {over ? <li style = {estiloLI}>{specie}</li> : null}
-      {over ? <li style = {listStyleNO}>{status}</li> : null}
-      {over ? <li style = {listStyleNO}>{status}</li> : null}
+      onClick={props.evento}
+      className='button'
+    >
+      {props.children}
     </div>
-   </div>
-   :
-   <div className="tarjetaMain" onMouseOver={filtrado}
-   onMouseLeave = {()=> setOver(false)}>
-    <div
-    className= 'tarjeta'>
-      <li style = {{listStyle: 'none'}}>{name}</li>
-      <img className = 'img-style' src = {imagen} alt= {`imagen de ${name}`}/>
-    </div>
-   </div>
-   
   )
 }
